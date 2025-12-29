f9f700f88bbc5c06c47908f4c87c9282b371b9d0
ErnestBeckham <ernestbeckham1998@gmail.com>
2025-12-29 14:07:24 +0530

    training under attack script added

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import grid2op
 from lightsim2grid import LightSimBackend
 from G_JEPA.trainer import JepaCM
+from grid2op.Reward.l2RPNSandBoxScore import L2RPNSandBoxScore
 
 
 env = grid2op.make("l2rpn_case14_sandbox", 
@@ -9,7 +10,8 @@ env = grid2op.make("l2rpn_case14_sandbox",
                 backend=LightSimBackend(),
                 #other_rewards={"loss": LossReward, "margin": MarginReward}
                    )
-
+# env_name = "rte_case5_example"
+# env = grid2op.make(env_name, test=True, reward_class=L2RPNSandBoxScore)
 
 jepa_config = {
     # === Encoder (state → latent) ===
@@ -29,6 +31,11 @@ jepa_config = {
     # === Optimizer & EMA ===
     "learning_rate":     1e-3,
     "tau":               0.99,            # EMA factor for target encoder
+    "final_jepa":"case5_jepa.pt",
+    "load_model":True,
+    "pred_num_blocks" :24,
+    "pred_mlp_dim":512,
+    "jepa_checkpoint_path":"G_JEPA/models"
 }
 
 
@@ -40,17 +47,33 @@ actor_config = {
     "learning_rate": 0.0003,
     "betas": (0.9, 0.999),
     "update_freq": 512,
-    "save_path":"G_JEPA\models",
-    'episodes': 100,
+    "save_path":"G_JEPA/models",
+    'checkpoint_path':"case5_actor_critic.pt",
+    'episodes': 5000,
+    'num_blocks' : 12,
     'max_ep_len':8063,
     'icm_lr':1e-4,
     'beta':1e-4,
     'alpha':1e-4,
     'batch_size':256,
     'intrinsic_reward_weight':1,
-    "episode_path":"G_JEPA\episode_length",
+    "episode_path":"G_JEPA/episode_length",
+    "load_model":True,
+    'actor_checkpoint_path':"models"
 }
 
 
-trainer = JepaCM(env, jepa_config, actor_config)
-trainer.train(start=0, end=500)
+def jepa_training(start=0, end=15):
+    trainer = JepaCM(env, jepa_config, actor_config)
+    trainer.train(start=0, end=15)
+
+
+def jepa_attack_training(start, end):
+    lines = [1, 3, 2, 5, 7, 8, 11, 19, 16]
+    trainer = JepaCM(env, jepa_config, actor_config)
+    trainer.train_attacked_env(start=start, end=end, line2attack=lines)
+
+
+if __name__ == "__main__":
+    jepa_attack_training(0, 15)
+    #jepa_attack_training(0, 10)
